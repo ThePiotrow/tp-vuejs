d897ac804aa73d9592bfeea73388655f73f84f82
Milan Camus <pro@turtletv.fr>
2022-12-16 17:02:19 +0100

    🚧 WIP `Field` component

---
+++ src/components/Field.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { inject, reactive, ref, defineProps } from "vue";
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  as: { default: "input", type: String, required: true },
+});
+
+const values = inject("values");
+</script>
+
+<template>
+  <component></component>
+</template>
